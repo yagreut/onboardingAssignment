3577ec3fa298dfc6d3084eb446034de151c94c6a
Reut Yagel <reut.yagel@aquasec.com>
2025-04-14 12:27:37 +0300

    basic json parsing logic

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+type RepoInput struct {
+	CloneURL string `json:"clone_url"`
+	SizeMB   int    `json:"size"` // in MB
+}
 
 func main() {
-	fmt.Println("Hello, Repo Scanner!")
+	// Open the input file
+	file, err := os.Open("input.json")
+	if err != nil {
+		logrus.WithError(err).Fatal("Failed to open input.json")
+	}
+	defer file.Close()
+
+	// Decode the JSON input
+	var input RepoInput
+	if err := json.NewDecoder(file).Decode(&input); err != nil {
+		logrus.WithError(err).Fatal("Failed to parse JSON input")
+	}
+
+	// Log the parsed data
+	logrus.WithFields(logrus.Fields{
+		"clone_url": input.CloneURL,
+		"size_MB":   input.SizeMB,
+	}).Info("Parsed input JSON successfully")
 }
